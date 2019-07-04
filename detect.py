da55104026b9e758b56fef33c637fffe8b8a8b28
kgazha <kgazha@gmail.com>
2019-07-04 16:44:35 +0500

    Add tests. Clear code in detect.py

--- detect.py
+++ detect.py
@@ -3,14 +3,9 @@ from os import listdir
 from os.path import isfile, join
 import cv2
 import numpy as np
-from PIL import Image
-from matplotlib import pyplot as plt
+
 
 def detect_image(filename, template_filename):
-    #f = open(filename, "rb")
-    #chunk = f.read()
-    #chunk_arr = np.frombuffer(chunk, dtype=np.uint8)
-    #img_rgb = cv2.imdecode(chunk_arr, cv2.IMREAD_COLOR)
     stream = open(filename, "rb")
     bytes = bytearray(stream.read())
     numpyarray = np.asarray(bytes, dtype=np.uint8)
@@ -20,25 +15,12 @@ def detect_image(filename, template_filename):
     height = int(img_rgb.shape[0] * 100 / img_rgb.shape[1])
     dim = (width, height)
     resized = cv2.resize(img_rgb, dim, interpolation=cv2.INTER_AREA)
-    # result = Image.fromarray((resized * 255).astype(np.uint8))
-    # result.save('temp.jpg')
-    img_gray =resized
-    # stream = open('temp.jpg', "rb")
-    # bytes = bytearray(stream.read())
-    # numpyarray = np.asarray(bytes, dtype=np.uint8)
-    # img_gray = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
+    img_gray = resized
 
-    #img_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread(template_filename, 0)
-    ################
     stream = open(template_filename, "rb")
     bytes = bytearray(stream.read())
     numpyarray = np.asarray(bytes, dtype=np.uint8)
     template = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
-    #############
-    # print('>>>>',img_rgb,'<<<<<<',template,'+++++')
-    #w, h = template.shape[::-1]
-    w, h = template.shape[0], template.shape[1]
 
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
@@ -47,21 +29,17 @@ def detect_image(filename, template_filename):
         return True
     return False
 
+
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.abspath(__file__))
     files = []
     template_filename = 'small_image.png'
     for f in listdir(current_dir):
-        if isfile(join(current_dir, f)) and f.lower().split('.')[-1] in ['gif', 'jpe', 'jpeg', 'jpg', 'png', 'svg', 'svgz'] \
-            and f.lower() != template_filename.lower():
+        if isfile(join(current_dir, f)) \
+                and f.lower().split('.')[-1] in ['gif', 'jpe', 'jpeg', 'jpg', 'png', 'svg', 'svgz'] \
+                and f.lower() != template_filename.lower():
             files.append(f)
     for file in files:
         result = detect_image(file, template_filename)
         if result:
-            print(file)
             break
-
-# for pt in zip(*loc[::-1]):
-    # cv2.rectangle(resized, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-# cv2.imwrite('res.png',resized)
--- tests.py
+++ tests.py
@@ -1,14 +1,16 @@
 import unittest
 from report import get_user_cv_from_db
+from report_sender import Report, User
+from unittest.mock import MagicMock
 
 
 class TestGetReport(unittest.TestCase):
     def test_get_user_cv_from_db(self):
         user_id = 2
         cv = get_user_cv_from_db(user_id)
-        true_values = {'fio': 'ПользовательАдминистратор',
+        true_values = {'fio': 'Пользователь Администратор Юзерович',
                        'contacts': '880553530; I.Aksenov@mininform74.ru',
-                       'date_of_birth': '802720800',
+                       'date_of_birth': '',
                        'citizenship': 'Россия',
                        'current_position': 'ОГБУ ЧРЦНИТ',
                        'current_activity_area': 'Поддержка moodle',
@@ -19,7 +21,7 @@ class TestGetReport(unittest.TestCase):
                        'management_experience_scale': 'небольшой',
                        'awards_presence': 'да',
                        'desired_authority_and_post_level': 'мининформ',
-                       'perspective_activity': '',
+                       'perspective_activity': 'Социальная политика; Информационные технологии ',
                        'family_status': 'Не замужем, не женат',
                        'children': 'нет',
                        'military_service': 'нет',
@@ -33,3 +35,11 @@ class TestGetReport(unittest.TestCase):
                        'criminal_record': 'отсутствует',
                        'willingness_state_secrets_access': 'отсутствует'}
         self.assertEqual(true_values, cv.__dict__)
+
+    def test_get_new_successful_users(self):
+        test_user_ids = [3, 49, 50, 67, 482]
+        test_users = [User(userid=_id) for _id in test_user_ids]
+        Report.get_successful_users = MagicMock(return_value=test_users)
+        Report.get_processed_user_ids = MagicMock(return_value=[3, 49])
+        new_successful_users = Report.get_new_successful_users()
+        self.assertEqual([user.userid for user in new_successful_users], [50, 67, 482])

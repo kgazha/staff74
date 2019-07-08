aaac72f3911a22817bcfc89402a5ffe163022b5c
kgazha <kgazha@gmail.com>
2019-07-08 15:43:21 +0500

    Fix the case of forming the wrong video path
    Signed-off-by: kgazha <kgazha@gmail.com>

--- user_files.sql
+++ user_files.sql
@@ -2,4 +2,17 @@ select max(id) as id, contenthash, max(CONCAT(contextid, '/', component, '/', fi
 json_agg(filearea) as filearea, json_agg(filename) as filename, json_agg(mimetype) as mimetype
 FROM mdl_files
 WHERE userid = '{0}' and filesize > 0
+and contenthash not in
+(select contenthash
+FROM mdl_files
+WHERE userid = '{0}' and filesize > 0
+and filearea like '%submission_files%'
+group by contenthash)
+group by contenthash
+union all
+select  max(id) as id, contenthash, max(CONCAT(contextid, '/', component, '/', filearea, '/', itemid, '/', filename)) as media_path,
+json_agg(filearea) as filearea, json_agg(filename) as filename, json_agg(mimetype) as mimetype
+FROM mdl_files
+WHERE userid = '{0}' and filesize > 0
+and filearea like '%submission_files%'
 group by contenthash
